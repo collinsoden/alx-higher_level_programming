15377297ffd7f90623c9a49c5f031ed7335da78d
Collinsoden22 <Collinsoden22@gmail.com>
2022-06-30 05:46:40 -0700

    Added task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     b = 5
     print("{} + {} = {}\n".format((a, b, calc.add(a, b))))
     print("{} - {} = {}\n".format((a, b, calc.sub(a, b))))
-    print("{} * {} = {}".format((a, b, calc.mul(a, b))))
-    print("{} / {} = {}".format((a, b, calc.div(a, b))))
+    print("{} * {} = {}\n".format((a, b, calc.mul(a, b))))
+    print("{} / {} = {}\n".format((a, b, calc.div(a, b))))
