7ee92fce4c6bf4dcf2812f84fe523316e13c6145
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2022-11-15 14:34:10 +0100

    Corrections

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,32 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
-  method: 'GET'
-};
-
-function getNameCharacter (url) {
-  return new Promise((resolve, reject) => {
-    request(url, function (err, res, body) {
-      if (err) {
-        // pass;
-      }
-      const json = JSON.parse(body);
-      resolve(json.name);
-    });
-  });
-}
-
-request(options, function (err, res, body) {
-  const json = JSON.parse(body);
-  async function getCharacters () {
-    for (const character of json.characters) {
-      const name = await getNameCharacter(character);
-      console.log(name);
-    }
-  }
-  getCharacters();
-  if (err) {
-    // pass;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
